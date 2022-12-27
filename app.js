fcc86877fa68643638231995a6f16e0c7c78e6f9
icodeatgit <codeatgit@gmail.com>
2022-12-27 16:06:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,328 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+
+// Load Middleware
+app.use(bodyparser.json());
+const jwt = require('jsonwebtoken');
+
+// CORS Middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+    next();
+});
+
+// check whether the request has a valid JWT access token
+let authenticate = (req, res, next) => {
+    let token = req.header('x-access-token');
+
+    // verify the JWT
+    jwt.verify(token, User.getJWTSecret(), (err, decoded) => {
+        if (err) {
+            // there was an error
+            // jwt is invalid - * DO NOT AUTHENTICATE *
+            res.status(401).send(err);
+        } else {
+            // jwt is valid
+            req.user_id = decoded._id;
+            next();
+        }
+    });
+}
+
+// Verify Refresh Token Middleware (which will be verifying the session)
+let verifySession = (req, res, next) => {
+    // grab the refresh token from the request header
+    let refreshToken = req.header('x-refresh-token');
+
+    // grab the _id from the request header
+    let _id = req.header('_id');
+
+    User.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            // user couldn't be found
+            return Promise.reject({
+                'error': 'User not found. Make sure that the refresh token and user id are correct'
+            });
+        }
+
+        // if the code reaches here - the user was found
+        // therefore the refresh token exists in the database - but we still have to check if it has expired or not
+
+        req.user_id = user._id;
+        req.userObject = user;
+        req.refreshToken = refreshToken;
+
+        let isSessionValid = false;
+
+        user.sessions.forEach((session) => {
+            if (session.token === refreshToken) {
+                // check if the session has expired
+                if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
+                    // refresh token has not expired
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            // the session is VALID - call next() to continue with processing this web request
+            next();
+        } else {
+            // the session is not valid
+            return Promise.reject({
+                'error': 'Refresh token has expired or the session is invalid'
+            })
+        }
+
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+}
+
+/* END MIDDLEWARE  */
+
+//Load in mongoose models
+const { List, Task, User, mongoose } = require('./db/models/index');
+
+////////////////////////////////////////////////////////////////////////////////
+/* Route Handlers */
+
+/* List Route Paths */
+
+/* 
+ * GET /lists
+ * Purpose : Get all lists
+ */
+app.get('/lists', authenticate, (req, res) => {
+    // We want to return an array of all the lists that belong to the authenticated user 
+    List.find({
+        _userId: req.user_id
+    }).then((lists) => {
+        res.send(lists);
+    }).catch((e) => {
+        res.send(e);
+    });
+})
+
+
+/* 
+ * POST /lists
+ * Purpose : Create a new list
+ */
+app.post('/lists', authenticate, (req, res) => {
+    let title = req.body.title;
+    let newlist = new List({
+        title,
+        _userId: req.user_id
+    });
+    newlist.save().then((listdoc) => {
+        res.send(listdoc);
+    })
+});
+
+/* 
+ * PATCH /lists:id
+ * Purpose : Update a specified list 
+ */
+app.patch('/lists/:id', (req, res) => {
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.sendStatus(200);
+    });
+});
+
+/* 
+ * DELETE /lists
+ * Purpose : Delete a specified list
+ */
+app.delete('/lists/:id', (req, res) => {
+    List.findOneAndRemove({ _id: req.params.id }).then(removedList => {
+        res.send(removedList);
+        // delete all the tasks that are in the deleted list
+        deleteTasksFromList(removedListDoc._id);
+    });
+});
+
+/* 
+ * GET /lists
+ * Purpose : Get all lists
+ */
+app.get('/lists/:id', (req, res) => {
+    List.findOne({ _id: req.params.id }).then(lists => {
+        res.send(lists);
+    });
+});
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/* Task Route Paths */
+
+/* 
+ * GET /lists/id/tasks
+ * Purpose : Get all Tasks from a specified list
+ */
+app.get('/lists/:listId/tasks', (req, res) => {
+    Task.find({ _listId: req.params.listId }).then(tasks => {
+        res.send(tasks);
+    });
+})
+
+/* 
+ * POST /lists/id/tasks
+ * Purpose : Create a new Task in the specified list
+ */
+app.post('/lists/:listId/tasks', (req, res) => {
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    })
+    newTask.save().then(newtaskdoc => {
+        res.send(newtaskdoc);
+    });
+});
+
+/* 
+ * PATCH /lists/id/tasks/taskid
+ * Purpose : Updates a Task in the specified list 
+ */
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }, {
+        $set: req.body
+    }).then(() => {
+        res.send({ message: "Updated Successfully"});
+    });
+});
+
+/* 
+ * DELETE /lists/id/tasks/taskid
+ * Purpose : deletes a task from a specified list
+ */
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((removedtask) => {
+        res.send(removedtask);
+    });
+});
+
+/* 
+ * GET /lists/listid/tasks/taskid
+ * Purpose : Get specified Task from a specified list
+ */
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOne({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((task) => {
+        res.send(task);
+    });
+});
+
+/* USER ROUTES */
+
+/**
+ * POST /users
+ * Purpose: Sign up
+ */
+ app.post('/users', (req, res) => {
+    // User sign up
+
+    let body = req.body;
+    let newUser = new User(body);
+
+    newUser.save().then(() => {
+        return newUser.createSession();
+    }).then((refreshToken) => {
+        // Session created successfully - refreshToken returned.
+        // now we geneate an access auth token for the user
+
+        return newUser.generateAccessAuthToken().then((accessToken) => {
+            // access auth token generated successfully, now we return an object containing the auth tokens
+            return { accessToken, refreshToken }
+        });
+    }).then((authTokens) => {
+        // Now we construct and send the response to the user with their auth tokens in the header and the user object in the body
+        res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(newUser);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+
+/**
+ * POST /users/login
+ * Purpose: Login
+ */
+ app.post('/users/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findByCredentials(email, password).then((user) => {
+        return user.createSession().then((refreshToken) => {
+            // Session created successfully - refreshToken returned.
+            // now we geneate an access auth token for the user
+
+            return user.generateAccessAuthToken().then((accessToken) => {
+                // access auth token generated successfully, now we return an object containing the auth tokens
+                return { accessToken, refreshToken }
+            });
+        }).then((authTokens) => {
+            // Now we construct and send the response to the user with their auth tokens in the header and the user object in the body
+            res
+                .header('x-refresh-token', authTokens.refreshToken)
+                .header('x-access-token', authTokens.accessToken)
+                .send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+/**
+ * GET /users/me/access-token
+ * Purpose: generates and returns an access token
+ */
+ app.get('/users/me/access-token', verifySession, (req, res) => {
+    // we know that the user/caller is authenticated and we have the user_id and user object available to us
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token', accessToken).send({ accessToken });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
+
+
+/* HELPER METHODS */
+let deleteTasksFromList = (_listId) => {
+    Task.deleteMany({
+        _listId
+    }).then(() => {
+        console.log("Tasks from " + _listId + " were deleted!");
+    })
+}
+
+
+/* Port to run on */
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Node is listening")
+});
